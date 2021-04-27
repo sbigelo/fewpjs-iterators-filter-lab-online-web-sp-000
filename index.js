4a0ff4cca2a54ca9f99dc1244467e30c356d3666
sbigelo <sean.bigelow1@gmail.com>
2021-04-27 16:58:22 -0400

    commit tree 2ddf5b39bace75ada8d685cf9a85db0178ae2984

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+
+function findMatching(drivers, string) {
+    return drivers.filter(x => x.toLowerCase().includes(string.toLowerCase()));
+
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(x => string.startsWith(x[0]))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(x => x.name === string)
+}
